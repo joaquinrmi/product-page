4c06ce023692dbe18dcee99fba9a699d406daf43
joaquinrmi <joaquinruaimi@gmail.com>
2022-07-10 22:47:02 -0300

    Fix Gallery component

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 
 import "./gallery.scss";
 
@@ -9,89 +9,13 @@ export interface Props
     thumbnail: Array<string>;
 }
 
-let moveGallery: (position: number) => Promise<void>;
-
-let moving = false;
-
 const Gallery: React.FunctionComponent<Props> = (props) =>
 {
-    let currentPosition = 0;
-
-    useEffect(() =>
-    {
-        const thumbnails = [
-            document.getElementById(`thumb-0-${props.id}`) as HTMLDivElement,
-            document.getElementById(`thumb-1-${props.id}`) as HTMLDivElement,
-            document.getElementById(`thumb-2-${props.id}`) as HTMLDivElement,
-            document.getElementById(`thumb-3-${props.id}`) as HTMLDivElement,
-        ];
-
-        moveGallery = async (position: number) =>
-        {
-            cardContainer.classList.remove(`pos${currentPosition}`);
-            cardContainer.classList.add(`pos${position}`);
-
-            thumbnails[currentPosition].classList.remove("active");
-            currentPosition = position;
-            thumbnails[currentPosition].classList.add("active");
-
-            return new Promise<void>(resolve =>
-            {
-                setTimeout(() =>
-                {
-                    buttonDelay = true;
-                    resolve();
-                },
-                500);
-            });
-        };
-
-        let buttonDelay = true;
-
-        const leftButton = document.getElementById(`left-${props.id}`) as HTMLDivElement;
-        const rightButton = document.getElementById(`right-${props.id}`) as HTMLDivElement;
-
-        const cardContainer = document.getElementById(`card-container-${props.id}`) as HTMLDivElement;
-
-        leftButton.onclick = () =>
-        {
-            if(currentPosition === 0)
-            {
-                return;
-            }
-
-            if(!buttonDelay)
-            {
-                return;
-            }
-            buttonDelay = false;
-
-            moveGallery(currentPosition - 1);
-        };
-
-        rightButton.onclick = () =>
-        {
-            if(currentPosition === props.images.length - 1)
-            {
-                return;
-            }
-
-            if(!buttonDelay)
-            {
-                return;
-            }
-            buttonDelay = false;
-
-            moveGallery(currentPosition + 1);
-        };
-    },
-    [
-        props.images
-    ]);
+    const [ currentPosition, setCurrentPosition ] = useState<number>(0);
 
     return <div id={props.id} className="gallery-container">
         <div className="image-slider">
-            <div id={`card-container-${props.id}`} className="card-container">
+            <div id={`card-container-${props.id}`} className={`card-container pos${currentPosition}`}>
                 {
                     props.images.map((element, index) =>
                     {
@@ -103,13 +27,43 @@ const Gallery: React.FunctionComponent<Props> = (props) =>
             </div>
 
             <div className="left-control-container">
-                <div id={`left-${props.id}`} className="control">
+                <div
+                    id={`left-${props.id}`}
+                    className="control"
+                    onClick={() =>
+                    {
+                        setCurrentPosition((position) =>
+                        {
+                            if(position === 0)
+                            {
+                                return position;
+                            }
+
+                            return position - 1;
+                        });
+                    }}
+                >
                     <i className="fi fi-rr-angle-small-left"></i>
                 </div>
             </div>
 
             <div className="right-control-container">
-                <div id={`right-${props.id}`} className="control">
+                <div
+                    id={`right-${props.id}`}
+                    className="control"
+                    onClick={() =>
+                    {
+                        setCurrentPosition((position) =>
+                        {
+                            if(position === props.images.length - 1)
+                            {
+                                return position;
+                            }
+
+                            return position + 1;
+                        });
+                    }}
+                >
                     <i className="fi fi-rr-angle-small-right"></i>
                 </div>
             </div>
@@ -119,17 +73,15 @@ const Gallery: React.FunctionComponent<Props> = (props) =>
             {
                 props.thumbnail.map((element, index) =>
                 {
-                    return <div key={`thumb-${index}`} id={`thumb-${index}-${props.id}`} className={`thumbnail ${index === 0 ? "active" : ""}`} onClick={async (ev) =>
-                    {
-                        if(moving)
+                    return <div
+                        key={`thumb-${index}`}
+                        id={`thumb-${index}-${props.id}`}
+                        className={`thumbnail ${index === currentPosition ? "active" : ""}`}
+                        onClick={async (ev) =>
                         {
-                            return;
-                        }
-                        moving = true;
-                        
-                        await moveGallery(index);
-                        moving = false;
-                    }}>
+                            
+                        }}
+                    >
                         <img src={element} alt="" />
                         <div className="white-screen"></div>
                     </div>;
